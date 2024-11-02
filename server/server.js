6cf1d631ec1e25b64dec7840ed91f360692c6649
maya <9999mayaakter@gmail.com>
2024-11-03 00:50:57 +0600

    edit env file and vercel

--- server/server.js
+++ server/server.js
@@ -11,10 +11,11 @@ import dashboardRouter from './Routes/dashboard_route.js';
 
 connectTodb();
 const app = express();
-app.use(cors({
-    origin:"https://ems-client-zqv2.vercel.app",
-    credentials:true
-}))
+// app.use(cors({
+//     origin:"https://ems-client-zqv2.vercel.app",
+//     credentials:true
+// }))
+app.use(cors());
     
 app.use(express.json());
 app.use(express.static('public/uploads'))
