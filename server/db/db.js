46607e243e3281b7a1e627bf7903e37758c36da0
maya <9999mayaakter@gmail.com>
2024-11-03 01:39:23 +0600

    change connectdb

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,24 @@
 import mongoose from "mongoose";
 
-const connectTodb=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGOURL)
-    } catch (error) {
-        console.log(error);
+// const connectTodb=async()=>{
+//     try {
+//         await mongoose.connect(process.env.MONGOURL)
+//     } catch (error) {
+//         console.log(error);
         
+//     }
+// }
+const connectTodb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGOURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1); // Exit the process if connection fails
     }
-}
+};
 
 export default connectTodb;
